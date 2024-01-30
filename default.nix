c52abda5afc00e2d5326532b6ccc99505b261ca2
geoff <geoff@generalconsulting.io>
2024-01-29 21:32:59 -0500

    pin

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
 let
+  nixpkgsVersion = {
+    url = "https://github.com/NixOS/nixpkgs/archive/90f456026d284c22b3e3497be980b2e47d0b28ac.tar.gz";
+    sha256 = "164lsq7xjjvpga6l6lfi9wfsnshgfxnpa8lvb2imscdwgmajakrc"; 
+  };
+
+  # Import the pinned nixpkgs
+  pkgs = import (builtins.fetchTarball nixpkgsVersion) { }; 
   nodejs = pkgs.deno;
   nvim = pkgs.neovim;
   make = pkgs.gnumake;
